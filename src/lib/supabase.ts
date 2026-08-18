0c0425487c66c1e0c9f3aa813c86c07dabde869e
karume17 <spiamicanal@gmail.com>
2026-08-18 00:32:10 -0500

    Creacion del Login

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://snyqgbzxchdtdgjlaohx.supabase.co";
-const supabaseAnonKey = "sb_publishable_u2lx3FwgX7X3V-X7FFcv4g_T1d9vPhp"; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
